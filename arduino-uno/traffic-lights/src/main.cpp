3e4a4470f12106a0017728e2e65dd5303d3d98a3
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2022-07-04 17:01:19 -0500

    add traffic light project for arduino uno

--- arduino-uno/traffic-lights/src/main.cpp
+++ arduino-uno/traffic-lights/src/main.cpp
@@ -1,32 +1,48 @@
 /**
- * Blink
+ * Traffic Lights
  *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
+ * Traffic lights simulator.
  */
 #include "Arduino.h"
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+int RED_LIGHT = 2;
+int YELLOW_LIGHT = 3;
+int GREEN_LIGHT = 4;
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize LEDs digital pin as an output.
+  pinMode(RED_LIGHT, OUTPUT);
+  pinMode(YELLOW_LIGHT, OUTPUT);
+  pinMode(GREEN_LIGHT, OUTPUT);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+  int operation_time = 5000;
+  int blink_times = 3;
+  int blink_time = 500;
 
-  // wait for a second
-  delay(1000);
+  // red light operation
+  digitalWrite(RED_LIGHT, HIGH);
+  digitalWrite(YELLOW_LIGHT, LOW);
+  digitalWrite(GREEN_LIGHT, LOW);
+  delay(operation_time);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+  // yellow light operation
+  digitalWrite(RED_LIGHT, LOW);
+  digitalWrite(GREEN_LIGHT, LOW);
+  for (int i = 0; i < blink_times; i++)
+  {
+    digitalWrite(YELLOW_LIGHT, HIGH);
+    delay(blink_time);
+    digitalWrite(YELLOW_LIGHT, LOW);
+    delay(blink_time);
+  }
 
-   // wait for a second
-  delay(1000);
+  // green light operation
+  digitalWrite(RED_LIGHT, LOW);
+  digitalWrite(YELLOW_LIGHT, LOW);
+  digitalWrite(GREEN_LIGHT, HIGH);
+  delay(operation_time);
 }
